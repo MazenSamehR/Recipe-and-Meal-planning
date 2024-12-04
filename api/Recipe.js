ab24f62aa72e7fd92aefd370e4cb6e268fe9b8d2
Mohanad <mohanadkhaled87@gmail.com>
2024-12-05 01:05:51 +0200

    add cloudinary for image uploading and user following/follower list

--- api/Recipe.js
+++ api/Recipe.js
@@ -22,8 +22,9 @@ const Recipe = require("../models/Recipe");
 
 router.get("/", async (req, res) => {
   try {
-    const recipes = await Recipe.find()
-      .select("title _id imageURL cooktime level") // Select only the required fields
+    const recipes = await Recipe.find().select(
+      "title _id imageURL cooktime level"
+    ); // Select only the required fields
     res.json({
       status: "SUCCESS",
       data: recipes,
@@ -37,18 +38,18 @@ router.get("/", async (req, res) => {
   }
 });
 
-
 router.get("/:id", async (req, res) => {
   try {
     const recipe = await Recipe.findById(req.params.id)
       .populate("chef", "username email")
       .populate({
-        path: "comments", 
+        path: "comments",
         populate: {
-          path: "author",  
-          select: "username" 
-        }
-      }).exec();
+          path: "author",
+          select: "username",
+        },
+      })
+      .exec();
 
     if (!recipe) {
       return res.status(404).json({
@@ -92,6 +93,74 @@ router.put("/recipes/:id", async (req, res) => {
     res.status(400).json({
       status: "FAILED",
       message: "Error updating recipe",
+      error: err.message,
+    });
+  }
+});
+
+router.put("/:userId/like/:id", async (req, res) => {
+  try {
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+      req.params.id,
+      { $inc: { likes: 1 } },
+      { new: true }
+    );
+    if (!updatedRecipe) {
+      return res.status(404).json({
+        status: "FAILED",
+        message: "Recipe not found",
+      });
+    }
+    res.json({
+      status: "SUCCESS",
+      message: "Recipe liked successfully",
+      data: updatedRecipe,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "FAILED",
+      message: "Error liking recipe",
+      error: err.message,
+    });
+  }
+});
+
+router.put("/:userId/dislike/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    if (!recipe) {
+      return res.status(404).json({
+        status: "FAILED",
+        message: "Recipe not found",
+      });
+    }
+    if (recipe.likes === 0) {
+      return res.status(400).json({
+        status: "FAILED",
+        message: "Recipe has no likes to remove",
+      });
+    }
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+      req.params.id,
+      { $inc: { likes: -1 } },
+      { new: true }
+    );
+
+    if (!updatedRecipe) {
+      return res.status(404).json({
+        status: "FAILED",
+        message: "Recipe not found",
+      });
+    }
+    res.json({
+      status: "SUCCESS",
+      message: "Recipe disliked successfully",
+      data: updatedRecipe,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "FAILED",
+      message: "Error disliking recipe",
       error: err.message,
     });
   }
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 require("./config/db");
 const cors = require("cors");
 
-const app = require('express')();
+const multer = require("multer");
+
+const fileUpload = multer();
+
+const app = require("express")();
 const port = process.env.PORT;
-const bodyParser = require('express').json;
+const bodyParser = require("express").json;
 const authRouter = require("./api/Auth");
 const RecipeRouter = require("./api/Recipe");
 const UserRouter = require("./api/User");
@@ -12,11 +16,13 @@ const CommentRouter = require("./api/Comment");
 
 app.use(cors());
 app.use(bodyParser());
+app.use(fileUpload.single("imageMessage"));
+
 app.use("/auth", authRouter);
 app.use("/recipe", RecipeRouter);
 app.use("/user", UserRouter);
 app.use("/comment", CommentRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}); 
+  console.log(`Server is running on port ${port}`);
+});
