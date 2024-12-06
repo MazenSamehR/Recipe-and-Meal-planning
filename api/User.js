0f0e1735b9089e9de3c7b0b03f6a2e41e5006891
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-12-06 15:50:14 +0200

    fix followers list

--- api/User.js
+++ api/User.js
@@ -21,7 +21,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id).populate(
-      "favoriteList followingList Recipes likeList"
+      "favoriteList followingList followerList Recipes likeList"
     );
     if (!user) return res.status(404).json({ message: "User not found" });
     res.json(user);
